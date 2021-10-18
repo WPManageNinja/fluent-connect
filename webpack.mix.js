d4864aa7e252c633f904939566e5ceadb9c4f532
Jewel <cep.jewel@gmail.com>
2021-10-18 20:23:45 +0600

    Added Fonts and index files to repo

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,4 +10,5 @@ mix
     .js('resources/admin/start.js', 'assets/admin/js/start.js').vue({ version: 3 })
     .js('resources/admin/global_admin.js', 'assets/admin/js/global_admin.js')
     .sass('resources/scss/admin.scss', 'assets/admin/css/admin.css')
-    .copy('resources/images', 'assets/images');
+    .copy('resources/images', 'assets/images')
+    .copy('resources/fonts', 'assets/admin/css/fonts');
